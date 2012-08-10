677259b721c9660c56bb2e568315ba83f9261616
Johnny Ray <johnny.ray02@gmail.com>
2012-08-10 10:13:53 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/*global require:false module:false Buffer:false*/
+/*global require:false module:false Buffer:false console:false*/
 
 var request = require('npm');
 var parser = require('libxml-to-js');
@@ -10,21 +10,35 @@ var crypto = require('crypto');
 var sf = require('sf');
 var builder = require('xmlbuilder');
 var aws_host = 'cloudfront.amazonaws.com';
-var required = ['secret_key', 'access_key', 'dist', 'resourcePaths'];
+var required = {
+  'secret_key': 'string', 
+  'access_key': 'string', 
+  'dist': 'string', 
+  'resourcePaths': 'object'
+};
 
 module.exports = function invalidate(opts, callback) {
   var missing = checkRequired(opts);
   if (missing.length > 0) {
+    if (opts.verbose) {
+      console.error('Required configuration is missing');
+    }
     return callback(new Error('Missing the following required config itmes ' + util.inspect(missing)));
   } else {
     var requestToSend = buildRequest(opts);
     request.post(requestToSend, function (err, resp, body) {
       if (err) {
+        if (opts.verbose) {
+          console.error('Request returned with error');
+        }
         return callback(err);
       }
 
       parser(body, function (error, result) {
         if (result.Error) {
+          if (opts.verbose) {
+            console.error('An error occured while parsing the response from AWS');
+          }
           error = error || new Error(result.Error.message);
         }
         return callback(error, resp.statusCode, result);
@@ -35,15 +49,24 @@ module.exports = function invalidate(opts, callback) {
 
 function checkRequired(opts) {
   var missing = [];
-  required.forEach(function (r) {
+  for (var r in required) {
     if (!opts[r]) {
       missing.push(r);
     }
-  });
+    if (r === 'resourcePaths') {
+      if (typeof opts[r] !== 'object') {
+        missing.push(r);
+        console.error('Configuration \'resourcePaths\' must be an array!');
+      }
+    }
+  }
   return missing;
 }
 
 function buildRequest(opts) {
+  if (opts.verbose) {
+    console.log('Building request');
+  }
   var resources = opts.resourcePaths;
   var doc = builder.create();
   var batch = doc.begin('InvalidationBatch');
@@ -68,12 +91,15 @@ function buildRequest(opts) {
 }
 
 function fetchHeaders(opts, contentLength) {
+  if (opts.verbose) {
+    console.log('Setting headers');
+  }
   var date = new Date().toGMTString();
   var hash = 
-    crypto
-    .createHmac('sha1', opts.secret_key)
-    .update(date, 'utf8')
-    .digest('binary');
+      crypto
+      .createHmac('sha1', opts.secret_key)
+      .update(date, 'utf8')
+      .digest('binary');
   var b64 = new Buffer(hash, 'binary').toString('base64');
   var auth = 'AWS ' + opts.access_key + ':' + b64;
 
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-
+var fs = require('fs');
 /*
   Required Configuration
 
   {
-    "resourcePaths": ["/jquery.js", "custom.js"],
+    "resourcePaths": ["/jquery.js", "/custom.js"],
     "secret_key": "mY$ecretKEY",
     "access_key": "accesskey",
     "dist": "target_distribution"
@@ -15,7 +15,6 @@ var invalidate = require('../index');
 
 fs.readFile('../../json/cloudfront.json', 'utf8', function (err, data) {
   var config = JSON.parse(data);
-
   invalidate(config, function (err, statusCode, body) {
     console.log(body);
   });
