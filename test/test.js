01c0f18fbbbe0d431d600d70a230d4c2cec88bd2
Johnny Ray <johnny.ray02@gmail.com>
2012-08-10 13:53:52 -0400

    vows for testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,43 @@
-var fs = require('fs');
-/*
-  Required Configuration
+'use strict';
+/*global require:false module:false console:false*/
 
-  {
-    "resourcePaths": ["/jquery.js", "/custom.js"],
-    "secret_key": "mY$ecretKEY",
-    "access_key": "accesskey",
-    "dist": "target_distribution"
-  }
-*/
-
+var vows = require('vows');
 var fs = require('fs');
+var sf = require('sf');
+var events = require('events');
+var assert = require('assert');
 var invalidate = require('../index');
+var assert = require('assert');
 
-fs.readFile('../../json/cloudfront.json', 'utf8', function (err, data) {
-  var config = JSON.parse(data);
-  invalidate(config, function (err, statusCode, body) {
-    console.log(body);
-  });
+vows.describe('Invalidator').addBatch({
+  'Using the invalidator': {
 
-});
+    'the response from the request': {
+      
+      topic: function () {
+        var file = fs.readFileSync('../../json/cloudfront.json', 'utf8');
+        var promise = new events.EventEmitter();
+        var config = JSON.parse(file);
+
+        invalidate(config, function (err, statusCode, body) {
+          promise.emit('success', err, statusCode, body);
+        });
+        return promise;
+      },
+
+      'no error occured': function (err, statusCode, body) {
+        assert.isNull(err);
+      },
+
+      'status code is correct': function (err, statusCode, body) {
+        assert.equal(statusCode, 201);
+      },
+
+      'response indicates invalidation in progress': function (err, statusCode, body) {
+        assert.equal(body.Status, 'InProgress');
+        assert.isNotNull(body.Id);
+      }
+    }
+  }
+}).export(module);
 
